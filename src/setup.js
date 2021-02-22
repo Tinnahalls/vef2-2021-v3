7fac8e0d63841645b63c4d53444763f019d928a9
Tinnahalls <tinnahalls@gmail.com>
2021-02-22 13:36:55 +0000

    added to setup and env document

--- src/setup.js
+++ src/setup.js
@@ -1,2 +1,21 @@
 import { promises } from 'fs';
 import faker from 'faker';
+
+import { query, end } from './db.js';
+
+
+const schemaFile = './sql/schema.sql';
+
+async function create() {
+  const data = await readFile(schemaFile);
+
+  await query(data.toString('utf-8'));
+
+  await end();
+
+  console.info('Schema created');
+}
+
+create().catch((err) => {
+  console.error('Error creating schema', err);
+});
